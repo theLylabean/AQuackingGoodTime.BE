3c5b943652504e22885031333ea53d216e3b2eca
Lyla Lynn <lyladlynn@gmail.com>
2025-08-06 19:16:09 -0500

    changed port

--- server.js
+++ server.js
@@ -3,7 +3,7 @@ dotenv.config();
 
 import app from './app.js';
 
-const PORT = process.env.PORT || 3000;
+const PORT = process.env.PORT || 3001;
 
 app.listen(PORT, () => {
   console.log(`Server is running on http://localhost:${PORT}`);
